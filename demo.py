78a3e58f44832d03e0ae064cd8f6c60c48e59346
VINAY-S-N <87693831+VINAY-S-N@users.noreply.github.com>
2022-03-18 13:15:18 +0530

    added fun

--- demo.py
+++ demo.py
@@ -1,19 +1,31 @@
-def add(a,b):
+def add(a, b):
     return a+b
+
 
 def sub(a, b):
     return a-b
 
-def mul(a,b):
-    return a*b
-def div(a,b):
-    return b/a
-def mod(a,b):
-    return a%b
 
-assert mod(1,10)==1
-assert add(5,5)==10
-assert div(1,1)==1
-assert sub(10,5)==5
-assert mul(4,6)==24
-#first project
+def mul(a, b):
+    return a*b
+
+
+def div(a, b):
+    return b/a
+
+
+def mod(a, b):
+    return a % b
+
+
+def inn(a):
+    return a+1
+
+
+inn(2)
+assert mod(1, 10) == 1
+assert add(5, 5) == 10
+assert div(1, 1) == 1
+assert sub(10, 5) == 5
+assert mul(4, 6) == 24
+# first project
